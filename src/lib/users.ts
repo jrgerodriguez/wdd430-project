e74f03b2b6c3e5ccfabd8009babc6386f4ab2767
Jorge Menjivar <menjivar.jorge.93@gmail.com>
2025-10-04 11:17:41 -0600

    updated 3

--- src/lib/users.ts
+++ src/lib/users.ts
@@ -4,7 +4,7 @@ import { User } from "@/types/user";
 export async function createNewUser(newUser: Omit<User, "id">): Promise<User> {
 
   const {data, error} = await supabase 
-    .from<"user", User>("user")
+    .from<"user", Omit<User, "id">>("user")
     .insert([newUser])
     .select()
     .single();
